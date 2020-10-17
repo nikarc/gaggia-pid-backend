cb900abf2e379a2c80768a492e86c0eac07be485
Nick Arcuri <narcuri1@gmail.com>
2020-10-16 21:31:20 -0400

    Initialize

---
+++ assets/precache-manifest.545168c09c7c06c8a557249e5303a614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fbbd350089ff5d5688b06e7d3c169f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b42a7717ffde766dcb3",
+    "url": "/static/css/main.9180a026.chunk.css"
+  },
+  {
+    "revision": "ccca8f490c23f3525beb",
+    "url": "/static/js/2.774a47b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.774a47b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b42a7717ffde766dcb3",
+    "url": "/static/js/main.f5aa4c13.chunk.js"
+  },
+  {
+    "revision": "a201181c7852b23a72a5",
+    "url": "/static/js/runtime-main.3efa572d.js"
+  }
+]);
